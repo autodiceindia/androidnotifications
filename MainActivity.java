4a65d4c1b40efc5e774afb6bfca94cc45417c093
autodiceindia <52938978+autodiceindia@users.noreply.github.com>
2019-08-20 11:27:44 +0530

    MainActivity Java File

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package in.autodice.notifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", FirebaseInstanceId.getInstance().getToken());
+    }
+}
