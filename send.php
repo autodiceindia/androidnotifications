edcc8288940e1f5ac55e83fee11a5df646588555
autodiceindia <52938978+autodiceindia@users.noreply.github.com>
2019-08-20 11:33:23 +0530

    PHP File

---
+++ send.php
@@ -0,0 +1,50 @@
+<?php
+function push_notification_android(){
+
+    /* Your App's Device Token */
+    $device_id="";
+    
+    $message="Your Message";
+
+    //API URL of FCM
+    $url = 'https://fcm.googleapis.com/fcm/send';
+
+    /* 
+      api_key available in:
+      Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
+     */    
+    $api_key = '*****************************************************';
+                
+    $fields = array (
+        'registration_ids' => array (
+                $device_id
+        ),
+        'data' => array (
+                "message" => $message
+        )
+    );
+
+    //header includes Content type and api key
+    $headers = array(
+        'Content-Type:application/json',
+        'Authorization:key='.$api_key
+    );
+                
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('FCM Send Error: ' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+}
+
+echo push_notification_android();
+?>
